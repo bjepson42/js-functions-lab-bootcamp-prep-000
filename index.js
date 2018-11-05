e290ae99dfeb0e7fb2ca8bc17e585fe3a2421fb2
Beau Jepson <beau.jepson@jepsonconsulting.com>
2018-11-05 23:35:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 function happyHolidays(){
-
     return "Happy holidays!"
 }// write your code below!
 
 function happyHolidaysTo(name){
-
     return 'Happy holidays, ${name}!'
-
 }
 
 function happyHolidayTo(holiday, name){
-
     return 'Happy ${holiday}, ${name}!'
 }
 
